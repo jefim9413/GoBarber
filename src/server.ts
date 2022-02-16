a31dd43325c6684cacd4061ed3be928cadacf1b6
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-02-15 22:29:43 -0300

    Iniciando Projeto

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333, () => {
-  console.log('server on');
+  console.log('🚀 Server started on port 3333!!');
 });
