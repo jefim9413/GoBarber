70b4a2f3720bb547eba2502f0cfcdfe744c75831
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-02-16 14:26:24 -0300

    Listando Agendamentos

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!!');
+  console.log('🚀 Server started on port 3333!');
 });
