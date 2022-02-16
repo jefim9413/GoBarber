7ec90dca116f5b65713fa579cd00dd6e7f65baf8
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-02-16 00:13:10 -0300

    Criando Modelo e Repositório

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,27 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repository/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
-  appointments.push(appointment);
+
+  const parserDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate =
+    appointmentsRepository.findByDate(parserDate);
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'this appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parserDate);
+
   return response.json(appointment);
 });
 
