19873a725eaa59932a115b56ffb950a9b3d41413
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-03-10 12:40:11 -0300

    Gerando token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { compare } from 'bcrypt';
 import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -8,6 +9,7 @@ interface Request {
 }
 interface Response {
   user: User;
+  token: string;
 }
 class AuthenticateUserService {
   public async execute({ email, password }: Request): Promise<Response> {
@@ -24,8 +26,13 @@ class AuthenticateUserService {
       throw new Error('Incorrenct email/password combination.');
     }
 
+    const token = sign({}, 'a726f0d95b04a20f24b39b40fa140914', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
     return {
       user,
+      token,
     };
   }
 }
