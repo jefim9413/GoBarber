01e809ec161a7635830740025953d60dfd8a3c52
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-03-28 22:09:11 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
